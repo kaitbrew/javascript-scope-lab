8ad79212664f4bfd6a65f536f2e0d7200f85f9e3
kait brewer <kaitlynmbrewer@gmail.com>
2025-05-11 22:07:26 -0400

    submitting this blind, could not get the auto test to run

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"];
+
+let featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+  const newBurger = "Flatburger";
+  burgers.push(newBurger);
+  return burgers;
+}
+
+
+if (true) {
+  {
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+  }
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+  return featuredDrink;
+}
